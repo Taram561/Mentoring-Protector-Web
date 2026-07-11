a6de61f00135df5cdaf7309813c85442267fd546
Taram561 <164663688+Taram561@users.noreply.github.com>
2026-07-11 13:54:16 +0300

    Add files via upload

---
+++ assets/Features.astro_astro_type_script_index_0_lang.CaN3KQfI.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./ScrollTrigger.DDi3XPDo.js";e.registerPlugin(t),e.utils.toArray(`.feature-row`).forEach(t=>{let n=t.querySelector(`.feature-visual`);e.from(t,{y:100,opacity:0,duration:1.5,ease:`power3.out`,scrollTrigger:{trigger:t,start:`top 80%`}}),n&&e.to(n,{y:-50,ease:`none`,scrollTrigger:{trigger:t,start:`top bottom`,end:`bottom top`,scrub:!0}})});
